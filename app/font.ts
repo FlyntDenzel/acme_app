74d4a2296f0931f591c561ac0fda6bacec6c8fbf
FlyntDenzel <flyntdenzel2004@gmail.com>
2024-03-21 19:39:03 +0100

    created a font.ts file to store font used for the app

---
+++ app/font.ts
@@ -0,0 +1,5 @@
+//file used to keep hold of all the fonts used during the creation of the app
+import { Inter } from 'next/font/google';
+
+// creating a subset for the Inter font: In this case latin
+export const inter = Inter({ subsets: ['latin'] });
