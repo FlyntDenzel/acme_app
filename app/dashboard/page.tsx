05420f014dd84557ce1d9c346e997645aec905b1
FlyntDenzel <flyntdenzel2004@gmail.com>
2024-03-22 21:31:04 +0100

    imported revenue charts and applied the graph to the dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,24 @@
 //homepage of the dashboard page i.e acme.com/dashboard
+import { Card } from '@/appui/dashboard/cards';
+import RevenueChart from '@/appui/dashboard/revenue-chart';
+import LatestInvoices from '@/appui/dashboard/latest-invoices';
+import { lusitana } from '@/appui/fonts';
+import { fetchRevenue } from '@/applib/data';
 
-export default function Page() {
-    return <p>Dashboard Page</p>
+export default async function Page() {
+    const revenue = await fetchRevenue();
+
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className='grid gap-6 sm:grid-cols-2 lg:grid-cols-4'>
+
+      </div>
+      <div className='mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8'>
+        <RevenueChart revenue={revenue}  />
+      </div>
+    </main>
+  );
 }
--- app/lib/placeholder-data.js
+++ app/lib/placeholder-data.js
@@ -111,7 +111,7 @@ const invoices = [
   },
   {
     customer_id: customers[6].id,
-    amount: 403,
+    amount: 666,
     status: 'pending',
     date: '2023-06-27',
   },
